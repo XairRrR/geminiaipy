72b6bc51d6a140792c00dbc0ad57c0499c69ace4
XairRrR <dev910382@gmail.com>
2025-02-17 10:40:43 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,16 @@
+import google.generativeai as ai
+
+API_KEY = 'AIzaSyCutOF-QATLnYRdErt5IotM6XaNPUFXSTk'
+
+ai.configure(api_key=API_KEY)
+
+model = ai.GenerativeModel("gemini-pro")
+chat = model.start_chat()
+
+while True:
+    message = input('Toi: ')
+    if message.lower() == 'bye':
+        print('Chatbot: Aurevoir !')
+        break
+    response = chat.send_message(message)
+    print('Chatbot:', response.text)
